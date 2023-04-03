2eb5162dfa17100bf0712c683710a56bd78894f4
sserve-kr <personal@sserve.me>
2023-04-03 22:22:09 +0900

    Add extendability

--- nyan_ext_translate/__init__.py
+++ nyan_ext_translate/__init__.py
@@ -1,6 +1,6 @@
 from nyanlang.helper import Helper, Param, ParamItem
 from nyanlang import return_
-from .translate import translate
+from .translate import Translator
 import sys
 from pathlib import Path
 
@@ -15,7 +15,9 @@ HELP = {
             ParamItem("bf", "Brainfuck"),
         ),
         Param("filename", "", no_desc=True),
-        Param("dest", "", no_desc=True, optional=True, kw="d")
+        Param("dest", "", no_desc=True, optional=True, kw="d"),
+        Param("stdout", "", no_desc=True, optional=True, kw="o"),
+        Param("stdin", "", no_desc=True, optional=True, kw="i")
     ),
 }
 
@@ -33,10 +35,16 @@ def run():
             return_(HELP["translate"])
         case [_, _, language, f, *options]:
             dest = None
+            stdout = False
+            stdin = False
             if "-d" in options:
                 dest = Path(options[options.index("-d") + 1]).absolute()
             elif "--dest" in options:
                 dest = Path(options[options.index("--dest") + 1]).absolute()
+            if "-o" in options or "--stdout" in options:
+                stdout = True
+            if "-i" in options or "--stdin" in options:
+                stdin = True
 
             if language not in SUPPORTED_LANGS:
                 raise ValueError(f"Invalid language {language}")
@@ -56,4 +64,7 @@ def run():
             if dest.exists():
                 raise FileExistsError(f"File {dest} already exists.")
 
-            translate(language, _f, dest)
+            t = Translator(language, _f, dest, stdout, stdin)
+            match language:
+                case "bf":
+                    t.brainfuck()
--- nyan_ext_translate/translate.py
+++ nyan_ext_translate/translate.py
@@ -1,31 +1,51 @@
 from pathlib import Path
+from sys import stdout, stdin
 
 
-def translate(lang, src: Path, dest: Path):
-    print(f"Translating '{src}' to '{dest}'...")
-    match lang:
-        case "bf":
-            mapper = {"<": "!", ">": "?", "+": "냥", "-": "냐", "[": "~", "]": "-", ",": ",", ".": ".", ' ': ' ',
-                      '\n': "\n"}
-            with open(src, "r") as _f:
-                origin_program = _f.read()
-            source = ""
-            comment_mode = False
-            for char in origin_program:
-                if char not in "<>+-[],. \n":
-                    if not comment_mode:
-                        source += "\""
-                    comment_mode = True
+class Translator:
+    def __init__(self, lang: str, src: Path, dest: Path, stdo: bool = False, stdi: bool = False):
+        self.lang = lang
+        self.src = src
+        self.dest = dest
+        self.stdout = stdo
+        self.stdin = stdi
+
+    def read(self):
+        if self.stdin:
+            return stdin.read()
+        with open(self.src, "r", encoding="utf-8") as _f:
+            return _f.read()
+
+    def write(self, source):
+        with open(self.dest, "w", encoding="utf-8") as _f:
+            _f.write(source)
+
+    def std_output(self, source):
+        stdout.write(source)
+
+    def brainfuck(self):
+        print(f"Translating '{self.src}' to '{self.dest}'...")
+        mapper = {"<": "!", ">": "?", "+": "냥", "-": "냐", "[": "~", "]": "-", ",": ",", ".": ".", ' ': ' ',
+                  '\n': "\n"}
+        origin_program = self.read()
+        source = ""
+        comment_mode = False
+        for char in origin_program:
+            if char not in "<>+-[],. \n":
+                if not comment_mode:
+                    source += "\""
+                comment_mode = True
+                source += char
+            else:
+                if char == " " and comment_mode:
                     source += char
-                else:
-                    if char == " " and comment_mode:
-                        source += char
-                        continue
-                    if comment_mode:
-                        comment_mode = False
-                        source += "\""
-                    source += mapper[char]
-            if comment_mode:
-                source += "\""
-            with open(dest, "w") as _f:
-                _f.write(source)
+                    continue
+                if comment_mode:
+                    comment_mode = False
+                    source += "\""
+                source += mapper[char]
+        if comment_mode:
+            source += "\""
+        self.write(source)
+        if self.stdout:
+            self.std_output(source)
